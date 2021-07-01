f5380870804244b5872af41439adcae089f2521c
Austin <austinborn212@gmail.com>
2021-07-01 06:17:17 -0700

    Complete testing

--- contracts/TheButton.sol
+++ contracts/TheButton.sol
@@ -16,13 +16,11 @@ contract TheButton {
   address public last_presser;
   uint public last_press_block;
 
-  event ButtonPressed(address, uint);
-  event Fallback(address, uint);
-  event Receive(address, uint);
+  event ButtonPressed(address);
   event TreasureClaimed(address, uint);
 
   /**
-   * @notice - Constructof which accepts a _press_fee argument
+   * @notice - Constructor which accepts a _press_fee argument
    * @param _press_fee - Fee to press button (nominated in wei)
    */
   constructor(uint _press_fee) {
@@ -30,33 +28,23 @@ contract TheButton {
   }
 
   function press_button() external payable {
-    // require(msg.value == press_fee, "Caller must pay fee to press the button.");
-    // require(msg.sender != last_presser, "Why would you press the button again??");
+    require(msg.value == press_fee, "Caller must pay exact fee to press the button.");
+    require(msg.sender != last_presser, "Why would you press the button again??");
 
     last_presser = msg.sender;
-    // last_press_block = block.number;
+    last_press_block = block.number;
 
-    // emit ButtonPressed(last_presser, last_press_block);
+    emit ButtonPressed(last_presser);
   }
 
   function claim_treasure() external {
     require(msg.sender == last_presser, "Caller did not press the button last.");
-    require(block.number >= last_press_block + TREASURE_WAIT, "Patience is a virtue.");
+    require(block.number > last_press_block + TREASURE_WAIT, "Patience is a virtue.");
 
     uint treasure = address(this).balance;
     (bool success,) = last_presser.call{value: treasure}("");
     require(success, "Attempt to send treasure failed.");
 
     emit TreasureClaimed(msg.sender, treasure);
-  }
-  
-  // Receive
-  receive() external payable {
-    emit Receive(msg.sender, msg.value);
-  }
-
-  // Fallback
-  fallback() external payable {
-    emit Fallback(msg.sender, msg.value);
   }
 }
--- test/the_button.js
+++ test/the_button.js
@@ -2,55 +2,189 @@ const TheButton = artifacts.require("TheButton");
 
 const constants = require('../constants');
 
+const advanceBlock = () => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "evm_mine",
+        id: new Date().getTime(),
+      },
+      (err, _) => {
+        if (err) {
+          return reject(err);
+        }
+        const newBlockHash = web3.eth.getBlock("latest").hash;
+        return resolve(newBlockHash);
+      },
+    );
+  });
+};
+
+const mapEvents = ({ event, returnValues }) => ({ event, returnValues });
+
 contract("TheButton", async accounts => {
-  it("Should initialize properly", async () => {
+  it("Initializes properly.", async () => {
     const instance = await TheButton.deployed();
+
     const press_fee = await instance.press_fee();
+    const last_presser = await instance.last_presser();
+    const last_press_block = await instance.last_press_block();
+    const events = await instance.getPastEvents();
+    const treasure = await web3.eth.getBalance(instance.address);
+
     assert.equal(press_fee, constants.PRESS_FEE);
+    assert.equal(last_presser, 0);
+    assert.equal(last_press_block, 0);
+    assert.deepEqual(events, []);
+    assert.equal(treasure, 0);
   });
 
-  // it("should call a function that depends on a linked library", async () => {
-  //   const meta = await MetaCoin.deployed();
-  //   const outCoinBalance = await meta.getBalance.call(accounts[0]);
-  //   const metaCoinBalance = outCoinBalance.toNumber();
-  //   const outCoinBalanceEth = await meta.getBalanceInEth.call(accounts[0]);
-  //   const metaCoinEthBalance = outCoinBalanceEth.toNumber();
-  //   assert.equal(metaCoinEthBalance, 2 * metaCoinBalance);
-  // });
+  it("Runs press_button() as expected.", async () => {
+    const instance = await TheButton.deployed();
 
-  // it("should send coin correctly", async () => {
-  //   // Get initial balances of first and second account.
-  //   const account_one = accounts[0];
-  //   const account_two = accounts[1];
-  //   let balance;
+    await instance.press_button({ from: accounts[0], value: constants.PRESS_FEE });
+    
+    const last_presser = await instance.last_presser();
+    const last_press_block = await instance.last_press_block();
+    const events = (await instance.getPastEvents()).map(mapEvents);
+    const treasure = await web3.eth.getBalance(instance.address);
+    const block_num = await web3.eth.getBlockNumber();
 
-  //   const amount = 10;
+    assert.equal(last_presser, accounts[0]);
+    assert.equal(last_press_block, block_num);
+    assert.deepEqual(events[0], {
+      event: 'ButtonPressed',
+      returnValues: { 0: accounts[0] }
+    });
+    assert.equal(treasure, constants.PRESS_FEE);
+  });
 
-  //   const instance = await MetaCoin.deployed();
-  //   const meta = instance;
+  it("Runs press_button() as expected again with different account.", async () => {
+    const instance = await TheButton.deployed();
 
-  //   balance = await meta.getBalance.call(account_one);
-  //   const account_one_starting_balance = balance.toNumber();
+    await instance.press_button({ from: accounts[1], value: constants.PRESS_FEE });
 
-  //   balance = await meta.getBalance.call(account_two);
-  //   const account_two_starting_balance = balance.toNumber();
-  //   await meta.sendCoin(account_two, amount, { from: account_one });
+    const last_presser = await instance.last_presser();
+    const last_press_block = await instance.last_press_block();
+    const events = (await instance.getPastEvents()).map(mapEvents);
+    const treasure = await web3.eth.getBalance(instance.address);
+    const block_num = await web3.eth.getBlockNumber();
 
-  //   balance = await meta.getBalance.call(account_one);
-  //   const account_one_ending_balance = balance.toNumber();
+    assert.equal(last_presser, accounts[1]);
+    assert.equal(last_press_block, block_num);
+    assert.deepEqual(events[0], {
+      event: 'ButtonPressed',
+      returnValues: { 0: accounts[1] }
+    });
+    assert.equal(treasure, constants.PRESS_FEE * 2);
+  });
 
-  //   balance = await meta.getBalance.call(account_two);
-  //   const account_two_ending_balance = balance.toNumber();
+  it("Rejects if same address calls press_button() again in a row.", async () => {
+    const instance = await TheButton.deployed();
 
-  //   assert.equal(
-  //     account_one_ending_balance,
-  //     account_one_starting_balance - amount,
-  //     "Amount wasn't correctly taken from the sender"
-  //   );
-  //   assert.equal(
-  //     account_two_ending_balance,
-  //     account_two_starting_balance + amount,
-  //     "Amount wasn't correctly sent to the receiver"
-  //   );
-  // });
+    try {
+      await instance.press_button({ from: accounts[1], value: constants.PRESS_FEE });
+      assert.fail();
+    } catch (e) {
+      assert(e.message.includes("Why would you press the button again??"));
+
+      const last_presser = await instance.last_presser();
+      const treasure = await web3.eth.getBalance(instance.address);
+
+      assert.equal(last_presser, accounts[1]);
+      assert.equal(treasure, constants.PRESS_FEE * 2);
+    }
+  });
+
+  it("Rejects if press_button() called with value < press_fee.", async () => {
+    const instance = await TheButton.deployed();
+
+    try {
+      await instance.press_button({ from: accounts[2], value: constants.PRESS_FEE - 1});
+      assert.fail();
+    } catch (e) {
+      assert(e.message.includes("Caller must pay exact fee to press the button."));
+
+      const last_presser = await instance.last_presser();
+      const treasure = await web3.eth.getBalance(instance.address);
+
+      assert.equal(last_presser, accounts[1]);
+      assert.equal(treasure, constants.PRESS_FEE * 2);
+    }
+  });
+
+  it("Rejects if press_button() called with value > press_fee.", async () => {
+    const instance = await TheButton.deployed();
+
+    try {
+      await instance.press_button({ from: accounts[2], value: constants.PRESS_FEE + 1});
+      assert.fail();
+    } catch (e) {
+      assert(e.message.includes("Caller must pay exact fee to press the button."));
+
+      const last_presser = await instance.last_presser();
+      const treasure = await web3.eth.getBalance(instance.address);
+
+      assert.equal(last_presser, accounts[1]);
+      assert.equal(treasure, constants.PRESS_FEE * 2);
+    }
+  });
+
+  it("Runs claim_treasure() as expected.", async () => {
+    const instance = await TheButton.deployed();
+
+    await advanceBlock();
+
+    const reward = await web3.eth.getBalance(instance.address);
+    const claimant_pre_balance = await web3.eth.getBalance(accounts[1]);
+
+    await instance.claim_treasure({ from: accounts[1] });
+    
+    const last_press_block = await instance.last_press_block();
+    const events = (await instance.getPastEvents()).map(mapEvents);
+    const treasure = await web3.eth.getBalance(instance.address);
+    const claimant_balance = await web3.eth.getBalance(accounts[1]);
+    const block = await web3.eth.getBlock("latest");
+    const gasPrice = await web3.eth.getGasPrice();
+
+    assert(last_press_block.toNumber() + constants.TREASURE_WAIT < block.number);
+    assert.equal(treasure, 0);
+    assert.equal(parseInt(claimant_balance), parseInt(claimant_pre_balance) + parseInt(reward) - block.gasUsed * parseInt(gasPrice));
+    assert.deepEqual(events[0], {
+      event: 'TreasureClaimed',
+      returnValues: { 0: accounts[1], 1: reward }
+    });
+  });
+
+  it("Rejects if claim_treasure() is called before enough blocks have passed.", async () => {
+    const instance = await TheButton.deployed();
+
+    try {
+      await instance.press_button({ from: accounts[0], value: constants.PRESS_FEE });
+      await instance.claim_treasure({ from : accounts[0] });
+      assert.fail();
+    } catch (e) {
+      assert(e.message.includes("Patience is a virtue."));
+
+      const treasure = await web3.eth.getBalance(instance.address);
+
+      assert.equal(treasure, constants.PRESS_FEE );
+    }
+  });
+
+  it("Rejects if claim_treasure() is called by address that is not last presser.", async () => {
+    const instance = await TheButton.deployed();
+
+    try {
+      await instance.claim_treasure({ from : accounts[1] });
+      assert.fail();
+    } catch (e) {
+      assert(e.message.includes("Caller did not press the button last."));
+
+      const treasure = await web3.eth.getBalance(instance.address);
+
+      assert.equal(treasure, constants.PRESS_FEE );
+    }
+  });
 });
