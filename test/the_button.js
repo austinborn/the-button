e44cdce86c7079b786d8e5ea86883ec0e2e1f126
Austin <austinborn212@gmail.com>
2021-07-01 06:27:06 -0700

    Modify tests

--- test/the_button.js
+++ test/the_button.js
@@ -2,25 +2,6 @@ const TheButton = artifacts.require("TheButton");
 
 const constants = require('../constants');
 
-const advanceBlock = () => {
-  return new Promise((resolve, reject) => {
-    web3.currentProvider.send(
-      {
-        jsonrpc: "2.0",
-        method: "evm_mine",
-        id: new Date().getTime(),
-      },
-      (err, _) => {
-        if (err) {
-          return reject(err);
-        }
-        const newBlockHash = web3.eth.getBlock("latest").hash;
-        return resolve(newBlockHash);
-      },
-    );
-  });
-};
-
 const mapEvents = ({ event, returnValues }) => ({ event, returnValues });
 
 contract("TheButton", async accounts => {
@@ -133,8 +114,6 @@ contract("TheButton", async accounts => {
 
   it("Runs claim_treasure() as expected.", async () => {
     const instance = await TheButton.deployed();
-
-    await advanceBlock();
 
     const reward = await web3.eth.getBalance(instance.address);
     const claimant_pre_balance = await web3.eth.getBalance(accounts[1]);
